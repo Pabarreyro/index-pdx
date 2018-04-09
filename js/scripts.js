7af9d6939472bc4e11bffd458b04346753083b05
Joy Paglinawan, Pablo Barreyro and julia sheremet <joy+pablo+julia@>
2018-04-09 16:35:22 -0700

    add nav bar expansion/collapse functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-$("#hamburger-nav-icon").click(function(){
-  $("#navBar-list").show();
+$(function() {
+  $("#hamburger-nav-icon").click(function(){
+    $("#filter-form").toggleClass("hide");
+  });
+
+  $("#filter-form").submit(function(event) {
+    event.preventDefault();
+
+    $("#filter-form").toggleClass("hide");
+  })
 });

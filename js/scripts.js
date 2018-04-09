c5ba6083e1c1b8d1e4ce37f0f94708fcab77bd5b
Adrian Messado and Brenda Franco <adrian@>
2018-04-09 14:31:22 -0700

    Add filter by neighborhood name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,75 @@
 // Business Logic
 
-// Place constructor
-function Place(name, price, neighborhood, type, diet, atmosphere, hours) {
+//Constructors
+function Place(name, price, type, diet, atmosphere, hours) {
   this.name = name;
   this.price = price;
-  this.neighborhood = neighborhood;
   this.type = type;
   this.diet = diet;
   this.atmosphere = atmosphere;
   this.hours = hours;
 }
 
-// Place Objects (Pearl)
-var powells = new Place("Powell's City of Books", "$$", "Pearl", ["shopping"], [], ["family-friendly", "casual", "book"], ["morning", "afternoon", "evening"]);
-var prasad = new Place("Prasad", "$$", "Pearl", ["food", "cafe"], ["vegan", "organic","raw", "gluten-free"], ["healthy", "casual"], ["early-morning", "afternoon"]);
+function Neighborhood(name='', places=[]) {
+  this.name = name;
+  this.places = places;
+}
 
-var chineseGarden = new Place("Lan Su Chinese Garden", "$$", "Downtown", ["garden", "parks"], ["tea"], ["family-friendly", "nature", "culture"], ["morning", "afternoon"]);
-var groundKontrol = new Place("Ground Kontrol", "$", "Downtown", ["entertainment", "bar"], ["gluten-free"], ["family-friendly", "arcade", "casual"], ["afternoon", "evening", "late-night"]);
-var departure = new Place("Departure Restaurent + Lounge", "$$", "Downtown", ["bar", "food"], ["gluten-free", "vegan"], ["upscale", "views"], ["evening", "late-night"]);
+// Place Objects (Pearl)
+var powells = new Place("Powell's City of Books",
+"$$",
+["shopping"],
+[],
+["family-friendly", "casual", "book"],
+["morning", "afternoon", "evening"]);
+
+var prasad = new Place("Prasad",
+"$$",
+["food", "cafe"],
+["vegan", "organic", "raw", "gluten-free"],
+["healthy", "casual"],
+["early-morning", "afternoon"]);
+
+// Place Objects (Downtown)
+var chineseGarden = new Place("Lan Su Chinese Garden",
+"$$",
+["garden", "parks"],
+["tea"],
+["family-friendly", "nature", "culture"],
+["morning", "afternoon"]);
+
+var groundKontrol = new Place("Ground Kontrol",
+"$",
+["entertainment", "bar"],
+["gluten-free"],
+["family-friendly", "arcade", "casual"],
+["afternoon", "evening", "late-night"]);
+
+var departure = new Place("Departure Restaurent + Lounge",
+"$$",
+["bar", "food"],
+["gluten-free", "vegan"],
+["upscale", "views"],
+["evening", "late-night"]);
+
+// Neighborhood objects
+var pearl = new Neighborhood("Pearl");
+pearl.places.push(powells, prasad);
+
+var downtown = new Neighborhood("Downtown");
+downtown.places.push(chineseGarden, groundKontrol, departure);
+
+
+// filter by neighborhoods name
+function filterByNeighborhoodName(userInput){
+  var neighborhoods = [pearl, downtown];
+  for(var i = 0; i < neighborhoods.length; i ++){
+    if(userInput === neighborhoods[i].name || neighborhoods[i].places.includes(userInput)){
+      return neighborhoods[i];
+    }
+  }
+  return false;
+}
+
+
+alert(filterByNeighborhoodName('Pearl'));
